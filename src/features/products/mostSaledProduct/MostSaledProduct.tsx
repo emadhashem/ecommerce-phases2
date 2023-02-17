e001855c8ec5c41c6dbe75584c201bed08115db3
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-17 23:59:30 +0200

    Added product style

--- src/features/products/mostSaledProduct/MostSaledProduct.tsx
+++ src/features/products/mostSaledProduct/MostSaledProduct.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import './mostSaledProduct.style.css'
-import imgSrc from '../../../assets/imgs/mainswiperImg.png'
+import React from "react";
+import "./mostSaledProduct.style.css";
+import imgSrc from "../../../assets/imgs/mainswiperImg.png";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+
 function MostSaledProduct() {
   return (
-    <div className='mostsaledproduct-container' >
-        <img className='product-img' src = {imgSrc} />
-        <div className='product-info' > 
-            test
+    <div className="mostsaledproduct-container">
+      <img className="product-img" src={imgSrc} alt="" />
+      <div className="product-footer">
+        <div className="product-icon">
+          <AddBoxIcon />
         </div>
+        <div className="product-info">
+          test
+          <span>$500</span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default MostSaledProduct
+export default MostSaledProduct;
