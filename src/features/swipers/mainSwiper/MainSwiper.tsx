3a8725b9f755955ae4d11cfc8b74293326b21fc1
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-18 00:01:05 +0200

    Added style & AutoPlay Func

--- src/features/swipers/mainSwiper/MainSwiper.tsx
+++ src/features/swipers/mainSwiper/MainSwiper.tsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import mainswiperImg from '../../../assets/imgs/mainswiperImg.png'
+import mainswiperImg from "../../../assets/imgs/mainswiperImg.png";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import { Navigation } from "swiper";
+import { Navigation, Autoplay } from "swiper";
 import "./mainSwiper.style.css";
 
 function MainSwiper() {
   return (
-   <div className="mainswiper-container" >
-     <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide>
-        <img src={mainswiperImg} />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={mainswiperImg} />
-      </SwiperSlide>
-    </Swiper>
-   </div>
+    <div className="mainswiper-container">
+      <Swiper
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        modules={[Autoplay, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={mainswiperImg} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={mainswiperImg} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
 
