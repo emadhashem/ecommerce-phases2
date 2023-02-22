0ac483c8e0a04797e499464dee6503c0512eb547
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-22 21:14:18 +0200

    Added loginPage style

--- src/pages/auth/loginPage/LoginPage.tsx
+++ src/pages/auth/loginPage/LoginPage.tsx
@@ -1,36 +1,92 @@
-import { Button, TextField } from "@mui/material";
-import React from "react";
+import { useState } from "react";
+import {
+  Button,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  FormControl
+} from "@mui/material";
+import "./loginPage.scss";
 import logo from "../../../assets/svgs/logo.svg";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import { Link } from "react-router-dom";
+
 function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   return (
-    <div>
-      <div>
+    <div className="loginPage">
+      <div className="logo-container">
         <img src={logo} />
         <p>تسجيل الدخول</p>
       </div>
-      <div>
-        <TextField
-          id="standard-basic"
-          label="Standard"
-          variant="standard"
-          placeholder="رقم الموبايل او البريد الالكتروني"
-        />
-        <TextField
-          id="standard-basic"
-          label="Standard"
-          variant="standard"
-          placeholder="كلمة المرور"
-        />
-      </div>
-      <div>
-        <Button variant="contained">تسجيل الدخول</Button>
-      </div>
-      <div>
-        <div>
-          <p>ليس لديك حساب</p>
-          <a>اشترك الان</a>
+      <div className="input-container">
+        <div className="input">
+          <FormControl sx={{ mt: 2, width: "333px" }} variant="outlined">
+            <InputLabel
+              className="label"
+              htmlFor="outlined-adornment-password"
+              sx={{ fontSize: "16px", color: "#2C7BE580", fontFamily: "Cairo" }}
+            >
+              رقم الموبايل او البريد الالكتروني
+            </InputLabel>
+            <OutlinedInput
+              sx={{ fontFamily: "Cairo" }}
+              id="outlined-basic"
+              label="رقم الموبايل او البريد الالكتروني"
+            />
+          </FormControl>
         </div>
-        <p>امان.سرعة.تنوع</p>
+        <div className="input">
+          <FormControl sx={{ mt: 2, width: "333px" }} variant="outlined">
+            <InputLabel
+              htmlFor="outlined-adornment-password"
+              sx={{ fontSize: "16px", color: "#2C7BE580", fontFamily: "Cairo" }}
+            >
+              كلمة المرور
+            </InputLabel>
+            <OutlinedInput
+              sx={{ fontFamily: "Cairo" }}
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="كلمة المرور"
+            />
+          </FormControl>
+        </div>
+      </div>
+      <div>
+        <Button className="btn" variant="contained">
+          <span>تسجيل الدخول</span>
+        </Button>
+      </div>
+      <div className="footer-container">
+        <span>ليس لديك حساب؟</span>
+        <Link to="/register" className="link">
+          <span>اشترك الان</span>
+        </Link>
+        <div className="footer-title">
+          <span>أمان , سرعة , تنوع </span>
+        </div>
       </div>
     </div>
   );
