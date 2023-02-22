7e5cd9cc28d765712ffb7a91edaaffcb48ec3b7f
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-22 22:09:18 +0200

    fix NavBar layout

--- src/layouts/navBar/NavBar.tsx
+++ src/layouts/navBar/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import useGlobalPath from "./useGlobalPath";
+import "./navBar.scss";
 
 function NavBar() {
   const CurNav = useGlobalPath(useLocation().pathname);
 
-  return <>{CurNav}</>;
+  return <div className="nav">{CurNav}</div>;
 }
 
 export default NavBar;
