4e094aeedee5cc7270443d19c33c7f30798729e5
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-22 21:14:38 +0200

    Added registerPage style

--- src/pages/auth/registerPage/RegisterPage.tsx
+++ src/pages/auth/registerPage/RegisterPage.tsx
@@ -1,28 +1,83 @@
 import { Avatar, Button, TextField } from "@mui/material";
-import React from "react";
-import logo from "../../../assets/svgs/logo.svg";
+import { Link } from "react-router-dom";
+import logo from "../../../assets/svgs/Vectorregister.svg";
+import "./registerPage.scss";
+
 function RegisterPage() {
   return (
-    <div>
-      <div>
-        <h3>تسجيل الاشتراك</h3>
-        <Avatar src={logo} sx={{ width: 56, height: 56}}/>
+    <div className="registerPage">
+      <div className="logo-container">
+        <span>تسجيل الاشتراك</span>
+        <div className="img-container">
+          <img src={logo} />
+          <span>صورة شحصية</span>
+        </div>
+      </div>
+      <div className="input-container">
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px" }}
+            required
+            placeholder="الاسم"
+          />
+        </div>
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px" }}
+            required
+            type="number"
+            placeholder="الهاتف"
+          />
+        </div>
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px" }}
+            required
+            type="email"
+            id="email"
+            placeholder="البريد"
+          />
+        </div>
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px" }}
+            required
+            type="password"
+            placeholder="كلمة المرور"
+          />
+        </div>
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px", color: "red" }}
+            required
+            type="password"
+            placeholder="تاكيد كلمة المرور "
+          />
+        </div>
+        <div className="input">
+          <TextField
+            sx={{ width: "351px", height: "40px" }}
+            required
+            type="address"
+            id="address"
+            placeholder="العنوان "
+          />
+        </div>
       </div>
       <div>
-        <TextField placeholder="الاسم" />
-        <TextField placeholder="الهاتف" />
-        <TextField placeholder="البريد" />
-        <TextField placeholder="كلمة المرور" />
-        <TextField placeholder="تاكيد كلمة المرور " />
-        <TextField placeholder="العنوان " />
+        <Button className="btn" variant="contained">
+          <span>تسجيل</span>
+        </Button>
       </div>
-      <div>
-        <Button>تسجيل</Button>
+      <div className="footer-container">
+        <span>هل لديك اشتراك بالفعل؟</span>
+        <div className="footer-title">
+          <span>يمكنك</span>
+          <Link to="/login" className="link">
+            <span> تسجيل الدخول</span>
+          </Link>
+        </div>
       </div>
-      <div>هل لديك اشتراك ؟</div>
-      <p>
-        يمكنك <a>تسجيل الدخول </a>
-      </p>
     </div>
   );
 }
