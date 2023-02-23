5c8ee5e8dc45dece650c14d597409ded3dee0894
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-23 14:45:40 +0200

    Added details

--- src/layouts/navBar/useGlobalPath.tsx
+++ src/layouts/navBar/useGlobalPath.tsx
@@ -3,14 +3,18 @@ import CartNavBar from "../../pages/cartPage/CartNavBar";
 import FavoriteNavBar from "../../pages/favorites/FavoriteNavBar";
 import MainNavBar from "../../pages/mainPage/MainNavBar";
 import NotificationNavBar from "../../pages/notifcationsPage/NotificationNavBar";
-const navbars = new Map<string , React.ReactNode>()
-navbars.set("/" , <MainNavBar />)
-navbars.set("/notifications" , <NotificationNavBar />)
-navbars.set("/cart" , <CartNavBar />)
-navbars.set("/favorites" , <FavoriteNavBar />)
-function useGlobalPath(curPath : string ) {
-    return navbars.get(curPath)
-}
+import DetailsNavBar from "../../pages/detailsPage/DetailsNavBar";
 
+const navbars = new Map<string, React.ReactNode>();
+navbars.set("/", <MainNavBar />);
+navbars.set("/notifications", <NotificationNavBar />);
+navbars.set("/cart", <CartNavBar />);
+navbars.set("/favorites", <FavoriteNavBar />);
+navbars.set("/details", <DetailsNavBar />);
+
+
+function useGlobalPath(curPath: string) {
+  return navbars.get(curPath);
+}
 
 export default useGlobalPath;
