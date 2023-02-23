f33b93f3e4627309e44c0abb04fab4d0ee872031
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-02-23 14:44:22 +0200

    Added cart footer section

--- src/pages/cartPage/CartPage.tsx
+++ src/pages/cartPage/CartPage.tsx
@@ -1,14 +1,15 @@
 import CartList from "../../features/cart/cartlist/CartList";
 import "./cartPage.scss";
 import DeleteIcon from "@mui/icons-material/Delete";
-
+import ArrowLeftRoundedIcon from "@mui/icons-material/ArrowLeftRounded";
+import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 
 function CartPage() {
   return (
     <div className="CartPage-container">
       <div className="container-title">
         <div className="title">
-          <div className="bar"></div>
+          <ArrowLeftRoundedIcon className="icon" />
           <h3>
             لديك <span>10</span> عناصر في السلة
           </h3>
@@ -18,6 +19,25 @@ function CartPage() {
         </div>
       </div>
       <CartList />
+      <div className="cart-footer">
+        <div className="price">
+          <div className="total-price">
+            <p>
+              المجموع: <span>9999999</span> ل.س
+            </p>
+          </div>
+          <div className="delivery">
+            <p>
+              توصيل: <span>9999</span> ل.س
+            </p>
+          </div>
+        </div>
+        <hr />
+        <button className="check-btn">
+          <CheckCircleRoundedIcon className="icon" />
+          <span>ارسال الطلب</span>
+        </button>
+      </div>
     </div>
   );
 }
