e946c0a7f2135b7c316c4cfba33b0622b5362440
ahmedzaki98 <ahmedzaki19998@gmail.com>
2023-03-03 18:59:22 +0200

    Updated register page img

--- src/pages/auth/registerPage/RegisterPage.tsx
+++ src/pages/auth/registerPage/RegisterPage.tsx
@@ -1,17 +1,42 @@
+import React, { useState, useRef } from "react";
 import { Avatar, Button, TextField } from "@mui/material";
 import { Link } from "react-router-dom";
 import logo from "../../../assets/svgs/Vectorregister.svg";
 import "./registerPage.scss";
 
 function RegisterPage() {
+  const [imgFile, setimgFile] = useState<any>();
+  const chooseFileRef = useRef<HTMLInputElement>(null);
+  async function onFileChoosen(eve: React.ChangeEvent<HTMLInputElement>) {
+    eve.stopPropagation();
+    eve.preventDefault();
+    const objectURL = await readBlob(eve.target.files![0]);
+    setimgFile(objectURL);
+  }
+  function onClickBtn() {
+    if (chooseFileRef.current) chooseFileRef.current.click();
+  }
+  async function readBlob(blob: File) {
+    const objectURL = URL.createObjectURL(blob);
+    return objectURL;
+  }
   return (
     <div className="registerPage">
       <div className="logo-container">
         <span>تسجيل الاشتراك</span>
-        <div className="img-container">
-          <img src={logo} />
-          <span>صورة شحصية</span>
-        </div>
+        <button className="edit-btn" onClick={onClickBtn}>
+          <span className="edit-btn-title">صورة شحصية</span>
+            <img className="selected-img" src={imgFile} alt=""  />
+          <div className="img-container">
+            {/* <img src={logo} /> */}
+            <input
+              ref={chooseFileRef}
+              type={"file"}
+              style={{ display: "none" }}
+              onChange={onFileChoosen}
+            />
+          </div>
+        </button>
       </div>
       <div className="input-container">
         <div className="input">
